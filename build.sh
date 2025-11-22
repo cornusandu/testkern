db76422ac04db5355c5614c96777c47b98938c89
Sandu Bogdan <cornusandu@hotmail.com>
2025-11-22 20:57:34 +0300

    .

--- build.sh
+++ build.sh
@@ -5,11 +5,18 @@ nasm -f elf32 ./src/bootseq/boot.asm -o boot.o
 nasm -f elf32 ./src/constants.asm -o constants.o
 nasm -f elf32 ./src/bootseq/stack.asm -o stack.o
 
-# C++ file — this is the only line that matters
-g++ -m32 -march=i386 -mtune=i386 -ffreestanding -nostdlib -fno-exceptions -fno-rtti -fno-asynchronous-unwind-tables -fno-unwind-tables -fno-pic -fno-PIE -c src/kernel_main.cpp -o kernel_main.o
+g++ -m32 -march=i386 -mtune=generic \
+    -O2 \
+    -ffreestanding -fno-stack-protector \
+    -fno-exceptions -fno-rtti -fno-asynchronous-unwind-tables \
+    -fno-unwind-tables -fno-pic -fno-PIE \
+    -mno-red-zone -mno-mmx -mno-sse -mno-sse2 \
+    -c src/kernel_main.cpp -o kernel_main.o
 
-# C file
-gcc -m32 -march=i386 -mtune=i386 -ffreestanding -nostdlib -fno-asynchronous-unwind-tables -c src/KEE.c -o KEE.o
+gcc -m32 -march=i386 \
+    -ffreestanding -fno-stack-protector \
+    -fno-asynchronous-unwind-tables \
+    -c src/KEE.c -o KEE.o
 
 ld -m elf_i386 -T src/linker.ld \
     entrypoint.o boot.o stack.o constants.o kernel_main.o KEE.o \
