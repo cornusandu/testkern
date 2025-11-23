49d0c7a5d502e3ec98fed4ecc6fff0bf7537c925
Sandu Bogdan <cornusandu@hotmail.com>
2025-11-23 20:09:02 +0200

    Fixed code

--- src/bootseq/boot.asm
+++ src/bootseq/boot.asm
@@ -1,23 +1,26 @@
 section .text
 global _boot
 _boot:
-    extern setup_stack
-    call setup_stack
+    ; Set up stack directly (no function call needed)
+    extern stack_bottom
+    lea esp, [stack_bottom - 4]  ; Point just below stack_bottom
+    and esp, 0xFFFFFFF0          ; 16-byte align
+    xor ebp, ebp
 
     ; Mask all interrupts on both PICs
     mov al, 0xFF
-    out 0x21, al    ; Master PIC data port
-    out 0xA1, al    ; Slave PIC data port
+    out 0x21, al
+    out 0xA1, al
 
     ; Switch to text mode
-    jmp skip_text  ; Temporarily disabled
+    jmp skip_text
 
-    ;mov ax, 0x0003  ; Commented for testing
+    ;mov ax, 0x0003
     ;int 0x10
 
     skip_text:
 
-    ;cli  ; Moved to entrypoint.asm
+    ;cli  ; Already in entrypoint.asm
 
     extern kernel_main
     call kernel_main
--- src/bootseq/stack.asm
+++ src/bootseq/stack.asm
@@ -2,13 +2,6 @@ section .data
 align 16
 global stack_top
 stack_top:
-    times 16384 db 0       ; 16 KiB of actual zeros in the .data section
+    times 16384 db 0       ; 16 KiB
+global stack_bottom
 stack_bottom:
-
-section .text
-global setup_stack
-setup_stack:
-    mov esp, stack_bottom
-    and esp, 0xFFFFFFF0   ; 16-byte align
-    xor ebp, ebp
-    ret
